87e1ef178db3e79e1e5359d4899ca359b2bade59
Siddhartha Laghuvarapu <siddhartha@aicrowd.com>
2021-10-27 13:14:13 +0530

    Initial commit

---
+++ rollout.py
@@ -0,0 +1,12 @@
+import aicrowd_gym
+
+# Create env and complete a few episodes
+env = aicrowd_gym.make("Acrobot-v1")
+env.reset()
+done = False
+scores = 0
+while not done:
+    obs, reward, done, _ = env.step(env.action_space.sample())
+    scores += reward
+
+print("Final scores:", scores)
---
+++ run.sh
@@ -0,0 +1,1 @@
+python rollout.py
