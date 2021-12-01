1c90fefbb5bf7975e223bfb3ccc292b0d85f7939
saketh2002 <na19b049@smail.iitm.ac.in>
2021-12-01 08:49:41 +0530

    changes omega

--- agent.py
+++ agent.py
@@ -2,7 +2,6 @@ import numpy
 from config import *
 import time
 import random
-
 """
 
 DO not modify the structure of "class Agent".
@@ -20,8 +19,6 @@ The final scoring is based on your agent's performance in this phase.
 Use the state saved in train phase here. 
 
 """
-
-
 ## order in configs: lr=0.9, discount rate=0.8 , eps=1, decay rate=0.005
 
 class Agent:
@@ -30,11 +27,11 @@ class Agent:
         self.env_name = env
 
         if self.env_name == 'acrobot':
-            self.q_acro = numpy.random.uniform(high=-3, low=0, size=(16, 16, 16, 16, 16, 16, 3))
+            self.q_acro = numpy.random.uniform(high = -3, low = 0, size = (16, 16, 16, 16, 16, 16, 3))
             self.config = config[self.env_name]
             self.episode_counts = 0
             self.curr_obs = None
-            self.avail_acro = [0, 1, 2]
+            self.avail_acro = [0,1,2]
             self.lr = 0.1
             self.disc_fact = 1
             self.eps = 1
@@ -43,14 +40,14 @@ class Agent:
             self.bucket_size = numpy.array([0.125, 0.125, 0.125, 0.125, 1.57079637, 3.53429174])
             self.curr_obs = None
             self.act = None
-            self.dummy1 = None
+            self.dummy2 = None
 
         if self.env_name == 'taxi':
-            self.q_taxi = numpy.zeros((500, 6))
+            self.q_taxi = numpy.zeros((500,6))
             self.config = config[self.env_name]
             self.episode_counts = 0
             self.curr_obs = None
-            self.avail_taxi = [0, 1, 2, 3, 4, 5]
+            self.avail_taxi = [0,1,2,3,4,5]
             self.lr = 0.8
             self.disc_r = 0.8
             self.eps = 1
@@ -58,11 +55,11 @@ class Agent:
             self.act = None
 
         if self.env_name == 'kbca':
-            self.q_kbca = numpy.zeros((50, 2))
+            self.q_kbca = numpy.zeros((50,2))
             self.config = config[self.env_name]
             self.episode_counts = 0
             self.curr_obs = None
-            self.avail_kbca = [0, 1]
+            self.avail_kbca = [0,1]
             self.lr = 0.9
             self.disc_r = 0.8
             self.eps = 1
@@ -70,11 +67,11 @@ class Agent:
             self.act = None
 
         if self.env_name == 'kbcb':
-            self.q_kbcb = numpy.zeros((50, 2))
+            self.q_kbcb = numpy.zeros((50,2))
             self.config = config[self.env_name]
             self.episode_counts = 0
             self.curr_obs = None
-            self.avail_kbcb = [0, 1]
+            self.avail_kbcb = [0,1]
             self.lr = 0.9
             self.disc_r = 0.8
             self.eps = 1.0
@@ -82,11 +79,11 @@ class Agent:
             self.act = None
 
         if self.env_name == 'kbcc':
-            self.q_kbcc = numpy.zeros((50, 3))
+            self.q_kbcc = numpy.zeros((50,3))
             self.config = config[self.env_name]
             self.episode_counts = 0
             self.curr_obs = None
-            self.avail_kbcc = [0, 1, 2]
+            self.avail_kbcc = [0,1,2]
             self.lr = 0.9
             self.disc_r = 0.8
             self.eps = 1.0
@@ -97,8 +94,7 @@ class Agent:
 
         if self.env_name == 'acrobot':
             action = random.choice([0, 2])
-            self.curr_obs = tuple(
-                numpy.clip(((obs - self.obs_space_low) / self.bucket_size).astype(numpy.int), None, 15))
+            self.curr_obs = tuple(numpy.clip(((obs - self.obs_space_low) / self.bucket_size).astype(numpy.int), None, 15))
             self.act = action
 
         if self.env_name == 'taxi':
@@ -128,14 +124,14 @@ class Agent:
 
     def compute_action_train(self, obs, reward, done, info):
 
+
         if self.env_name == 'acrobot':
 
-            discrete_obs = tuple(
-                numpy.clip(((obs - self.obs_space_low) / self.bucket_size).astype(numpy.int), None, 15))
+            discrete_obs = tuple(numpy.clip(((obs - self.obs_space_low) / self.bucket_size).astype(numpy.int), None, 15))
+
 
             self.q_acro[self.curr_obs][self.act] = self.q_acro[self.curr_obs][self.act] + self.lr * (
-                    reward + self.disc_fact * numpy.max(self.q_acro[discrete_obs]) - self.q_acro[self.curr_obs][
-                self.act])
+                    reward + self.disc_fact * numpy.max(self.q_acro[discrete_obs]) - self.q_acro[self.curr_obs][self.act])
 
             if random.uniform(0, 1) < self.eps and obs[1] <= 0 and obs[4] < 0:
                 action = 2
@@ -153,13 +149,14 @@ class Agent:
                 # get action based on current q_table
                 action = numpy.argmax(self.q_acro[discrete_obs])
 
+
             self.curr_obs = discrete_obs
             self.act = action
 
             if done:
                 self.episode_counts += 1
-                #     self.eps_end_decay = (3 * self.episode_counts) // 4
-                #     self.eps_decay_rate = self.eps / (self.eps_end_decay - self.eps_start_decay)
+          #     self.eps_end_decay = (3 * self.episode_counts) // 4
+          #     self.eps_decay_rate = self.eps / (self.eps_end_decay - self.eps_start_decay)
                 if 1 <= self.episode_counts < 2000:
                     self.eps -= self.eps_decay_rate
 
@@ -199,15 +196,15 @@ class Agent:
 
             if random.uniform(0, 1) < self.eps and -29 <= new_obs <= -8:
 
-                action = random.choices(self.avail_kbca, weights=[0.0, 1.0], k=1)[0]
+                action = random.choices(self.avail_kbca, weights = [0.0, 1.0], k = 1)[0]
 
             elif random.uniform(0, 1) < self.eps and -8 < new_obs <= 4:
 
-                action = random.choices(self.avail_kbca, weights=[0.2, 0.8], k=1)[0]
+                action = random.choices(self.avail_kbca, weights = [0.2, 0.8], k = 1)[0]
 
             elif random.uniform(0, 1) < self.eps and 4 < new_obs <= 16:
 
-                action = random.choices(self.avail_kbca, weights=[0.8, 0.2], k=1)[0]
+                action = random.choices(self.avail_kbca, weights = [0.8, 0.2], k = 1)[0]
 
             else:
                 # exploit
@@ -218,6 +215,7 @@ class Agent:
 
             # Q-learning algorithm
             #    self.q_taxi[obs,action] = self.q_taxi[obs,action] + learning_rate * (reward + discount_rate * np.max(self.q_taxi[new_obs,:])-self.q_taxi[obs,action])
+
 
             # Update to our new state
             #    obs = new_obs
@@ -261,6 +259,7 @@ class Agent:
             # Q-learning algorithm
             #    self.q_taxi[obs,action] = self.q_taxi[obs,action] + learning_rate * (reward + discount_rate * np.max(self.q_taxi[new_obs,:])-self.q_taxi[obs,action])
 
+
             # Update to our new state
             #    obs = new_obs
             self.curr_obs = new_obs
@@ -287,7 +286,7 @@ class Agent:
 
             elif random.uniform(0, 1) < self.eps and -8 < new_obs <= 4:
 
-                action = random.choices([0, 1], weights=[0.2, 0.8], k=1)[0]
+                action = random.choices([0,1], weights=[0.2, 0.8], k=1)[0]
 
             elif random.uniform(0, 1) < self.eps and 4 < new_obs <= 16:
 
@@ -303,6 +302,7 @@ class Agent:
             # Q-learning algorithm
             #    self.q_taxi[obs,action] = self.q_taxi[obs,action] + learning_rate * (reward + discount_rate * np.max(self.q_taxi[new_obs,:])-self.q_taxi[obs,action])
 
+
             # Update to our new state
             #    obs = new_obs
             self.curr_obs = new_obs
@@ -314,6 +314,9 @@ class Agent:
                 self.eps = numpy.exp(-self.decay_r * self.episode_counts)
 
             return action
+
+
+
 
     def register_reset_test(self, obs):
 
@@ -329,6 +332,7 @@ class Agent:
 
             elif obs[1] > 0 and obs[4] <= 0:
                 action = 2
+
 
         if self.env_name == 'taxi':
             action = int(numpy.argmax(self.q_taxi[obs]))
@@ -347,8 +351,8 @@ class Agent:
     def compute_action_test(self, obs, reward, done, info):
 
         if self.env_name == 'acrobot':
-            #   discrete_obs = tuple(numpy.clip(((obs - self.obs_space_low) / self.bucket_size).astype(numpy.int), None, 15))
-            #   action = numpy.argmax(self.q_acro[discrete_obs])
+        #   discrete_obs = tuple(numpy.clip(((obs - self.obs_space_low) / self.bucket_size).astype(numpy.int), None, 15))
+        #   action = numpy.argmax(self.q_acro[discrete_obs])
             if obs[1] <= 0 and obs[4] < 0:
                 action = 2
 
